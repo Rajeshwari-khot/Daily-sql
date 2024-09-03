423eb7594ccdd41cec5c6289ea6d65ec071e71f4
Rajeshwari-khot <59497425+Rajeshwari-khot@users.noreply.github.com>
2024-09-03 23:55:56 +0530

    Update 1_avg_selling_price.sql

--- 1_avg_selling_price.sql
+++ 1_avg_selling_price.sql
@@ -1,9 +1,1 @@
-
-
-select product_id,round(sum(total_price)/sum(units),2)as average_price
-from (
-select p.product_id,p.price,u.units,p.price*u.units as total_price
-from prices p join unitssold u
-on p.product_id=u.product_id
-where u.purchase_date between p.start_date and p.end_date)as b
-group by product_id
+SELECT p.product_id,coalesce(ROUND(SUM(p.price*u.units) * 1.0 / SUM(u.units), 2),0) AS average_price FROM Prices p left JOIN UnitsSold u ON (p.product_id = u.product_id) AND (u.purchase_date BETWEEN p.start_date AND p.end_date)GROUP BY p.product_id
