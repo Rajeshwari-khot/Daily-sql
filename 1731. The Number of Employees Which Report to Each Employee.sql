0ed7806b7107a921a164d635b267dbf518c5879b
Rajeshwari-khot <59497425+Rajeshwari-khot@users.noreply.github.com>
2024-09-05 23:49:55 +0530

    Create 1731. The Number of Employees Which Report to Each Employee.sql

---
+++ 1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,6 @@
+select e.employee_id as employee_id, e.name , count(r.reports_to) as  reports_count , round(avg(r.age)) as average_age
+from employees e
+inner join employees r
+on e.employee_id = r.reports_to
+group by e.employee_id,e.name
+order by e.employee_id;
